5589dd53c4dba1d0731eb88aae14ad4ed74069f6
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2024-04-15 13:49:21 +0200

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,7 @@ import React from 'react';
 const Page: React.FC = () => {
   return (
 
-    <html lang="en">
-    <head>
-      
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Omega CZ</title>
-    </head>
-    <body>
+    
     <div>
       <h1>Welcome to My Page</h1>
       <p>This is a basic page created with React.</p>
@@ -24,8 +18,7 @@ const Page: React.FC = () => {
       </p>
     </div>
       
-    </body>
-    </html>
+
     
   );
 };
