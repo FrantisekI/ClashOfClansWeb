5be39e4e4a2bc7be41ff6f98735cfe8ae612ee22
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2024-04-17 16:46:13 +0200

    modified:   app/globals.css modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,41 @@ const Page: React.FC = () => {
 
 
     <div className='main'>
-      <div className='section'>
+
+      
 
 
-        <h1>Welcome to My Page</h1>
-        <p>This is a basic page created with React.</p>
-        <br />
 
-        
-      </div>
-      <div className='section'>
-      <p>
-          Příliš žluťoučký kůň úpěl ďábelské ódy.
-        </p><p>
-          PŘÍLIŠ ŽLUŤOUČKÝ KŮŇ ÚPĚL ĎÁBELSKÉ ÓDY.
 
-        </p>
+        <div className='section'>
+          <p>Jsme přátelský a pohodový klan, který uvítá aktivní hráče mobilní hry 
+             <a href="https://supercell.com/en/games/clashofclans/" target="_blank" rel="noopener">
+              Clash of Clans</a>.
+               Máme zájem i o nováčky, kterým ochotně poradíme a rádi pomůžeme k jejich postupu a vývoji.
+               </p>
         </div>
+
+
+
+
+        <div className='section'><p>Klanových válek se účastníme téměř nonstop a často vítězíme. V klanových hrách náš tým získává nejvyšší výhry. Díky bojům v Raid Weekend každé pondělí dostává každý člen klanu slušnou sumu raidových medailí pro vylepšování vesnice.</p></div>
+
+        <div className='section'>
+          <p>Jestli máte zájem o 
+            <a href="https://link.clashofclans.com/en?action=OpenClanProfile&amp;tag=2PG29LY8J" 
+            target="_blank" rel="noopener">vstup do našeho klanu</a>
+          , seznamte se důkladně s 
+          <a href="https://omegacz.clanweb.eu/titulka/pravidla-klanu/">pravidly klanu</a>, 
+          která jsou pro každého člena závazná. Žádosti o vstup do našeho klanu akceptujeme pouze
+           v češtině nebo slovenštině.</p>
+           </div>
+        <div className='section'><p>Převážně je členský stav klanu&nbsp; naplněný. V případě silné motivace být členem našeho klanu se ozvěte na <a href="https://discord.gg/K5XZFHXXtt" target="_blank" rel="noopener">našem Discordu</a>. Volné místo v rámci možností vytvoříme.</p></div>
+        <div className='section'><p>Od dubna 2023 jsme se rozšířili o sekundární klan <a href="https://link.clashofclans.com/en?action=OpenClanProfile&amp;tag=2QJL8LULJ" target="_blank" rel="noopener">OdpadliciCZ</a>, primárně určený na pořádání klanové bojové ligy slabších radnic, které se nedostanou do Top 30 v hlavním klanu. Mimo ligovou sezónu slouží jako akademie pro méně aktivní hráče nebo nezkušené bojovníky.</p>
+        </div>
+
+
+
+      
     </div>
 
 
